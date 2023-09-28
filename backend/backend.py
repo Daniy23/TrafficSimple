9398afcf73a67ae78aaa6717f73b9963511f5760
Daniela Lozada <a01736594@tec.mx>
2023-09-28 14:55:32 -0600

    FIN

---
+++ backend/backend.py
@@ -0,0 +1,36 @@
+import flask
+from flask.json import jsonify
+import uuid
+from traffic import Street
+
+games = {}
+
+app = flask.Flask(__name__)
+
+@app.route("/games", methods=["POST"])
+def create():
+    global games
+    id = str(uuid.uuid4())
+    games[id] = Street()
+    lista = []
+    for ghost in games[id].schedule.agents:
+        lista.append({"id": ghost.unique_id, "x": int(ghost.pos[0]), "z": int(ghost.pos[1]), "speed": [float(ghost.speed[0]), 0.0, 0.0]})
+
+    #response = flask.make_response()
+    #response.headers['Location'] = f"/games/{id}"
+    #response.status_code = 201
+    #response.data = jsonify(lista)
+    #return response
+    return jsonify(lista), 201, {'Location': f"/games/{id}"}
+
+@app.route("/games/<id>", methods=["GET"])
+def queryState(id):
+    global model
+    model = games[id]
+    model.step()
+    lista = []
+    for ghost in model.schedule.agents:
+        lista.append({"id": ghost.unique_id, "x": int(ghost.pos[0]), "z": int(ghost.pos[1]), "speed": [float(ghost.speed[0]), 0.0, 0.0]})
+    return jsonify(lista)
+
+app.run()
