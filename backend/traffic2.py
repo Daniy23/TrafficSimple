7a1aaae9829e5bb9519bc11402f8777bd3376f41
Daniela Lozada <a01736594@tec.mx>
2023-10-05 22:04:31 -0600

    Implementación de vector de dirección y multiples agentes

--- backend/traffic2.py
+++ backend/traffic2.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-import random
-
 from mesa import Agent, Model
 from mesa.space import ContinuousSpace
 from mesa.time import RandomActivation
@@ -9,95 +7,80 @@ from mesa.visualization.ModularVisualization import ModularServer
 
 from SimpleContinuousModule import SimpleCanvas
 
-from pathfinding.core.grid import Grid
-from mesa.space import SingleGrid
-
-from pathfinding.core.diagonal_movement import DiagonalMovement
-from pathfinding.finder.a_star import AStarFinder
 
 class Car(Agent):
-    def __init__(self, model: Model, pos, posfinal):
+    
+    #No importa la direcccion a la que empiecen, cuando se encuentra en un punto critico cambia
+    posc = [6,10]
+    posc1 = [6,5]
+    def __init__(self, model: Model, pos, speed):
         super().__init__(model.next_id(), model)
         self.pos = pos
-        self.posfinal = posfinal
-
-    def step(self):
-        matrix = [
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-        [0,0,0,0,1,1,1,1,1,1,1,0,0,0,0],
-        [0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
-        [1,1,1,1,1,0,0,0,0,0,1,1,1,1,1],
-        [0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
-        [1,1,1,1,1,0,0,0,0,0,1,1,1,1,1],
-        [0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
-        [0,0,0,0,1,1,1,1,1,1,1,0,0,0,0],
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0]]
-        
-        grid = Grid(matrix=matrix)
-        (x,y) =self.pos
-        (a,b) = self.posfinal
-        
-        start = grid.node(x, y)
-        end = grid.node(a, b)
-        
-        finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
-        path, runs = finder.find_path(start, end, grid)
-
-        print('operations:', runs, 'path length:', len(path))
-        print(grid.grid_str(path=path, start=start, end=end)) 
-        self.model.grid.move_agent(self, (path[1].x, path[1].y))
+        self.speed = speed
     
+
+
+    def step(self):   
+        x, y = self.pos  
+        direccionAbajo = [0,0.5] 
+        if (x == self.posc[0]) and (y == self.posc[1]):
+            self.speed = np.array(direccionAbajo)
+        elif (x == self.posc1[0]) and (y == self.posc1[1]):
+            self.speed = np.array(direccionAbajo)
+        new_pos = self.pos + np.array([1,1]) * self.speed
+
+        
+        if not self.agent_position(new_pos[0], new_pos[1]): 
+            self.model.space.move_agent(self, new_pos)  
+    
+            
+    def agent_position(self, x, y):
+            agents = self.model.space.get_neighbors((x, y), radius =0, include_center = True)
+            return any(isinstance(agent, Car) for agent in agents)
+            
+
+
+
 class Street(Model):
     def __init__(self):
         super().__init__()
-        self.space = ContinuousSpace(15, 15, True)
-        self.grid = SingleGrid(15, 15, torus=True)
-
+        self.space = ContinuousSpace(25, 25, True)
         self.schedule = RandomActivation(self)
-        matrix = [
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-        [0,0,0,0,1,1,1,1,1,1,1,0,0,0,0],
-        [0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
-        [1,1,1,1,1,0,0,0,0,0,1,1,1,1,1],
-        [0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
-        [1,1,1,1,1,0,0,0,0,0,1,1,1,1,1],
-        [0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
-        [0,0,0,0,1,1,1,1,1,1,1,0,0,0,0],
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,0,1,0,0,0,0,0,0]]
-        
-    
 
-        car = Car(self, (6, 0), (0, 6))
-        self.grid.place_agent(car, car.pos)
-        self.schedule.add(car)
+#CANTIDAD DE CARROS
+        ncarros = 4
+#Generación de carros lado A YAAA ESTA 
+        # a = 5
         
-        # car = Car(self, (0,6), (0, 8))
-        # self.grid.place_agent(car, car.pos)
-        # self.schedule.add(car)
-        
-        car2 = Car(self, (6, 14), (0, 8))
-        self.grid.place_agent(car2, car2.pos)
-        self.schedule.add(car2)
+        # for px in np.random.choice(1, ncarros, replace=True):
+        #     car = Car(self, np.array([a, px]), np.array([0.0, 1.0]))
+        #     self.space.place_agent(car, car.pos)
+        #     self.schedule.add(car)
+# #Generación de carros lado B
+        b = 5
+        for px in np.random.choice(1, ncarros, replace=True):
+            car = Car(self, np.array([px, b]), np.array([1.0, 0.0]))
+            self.space.place_agent(car, car.pos)
+            self.schedule.add(car)
+#Generación de carros lado C YAA ESTA 
+        # c = 10
+        # for px in np.random.choice(1, ncarros, replace=True):
+        #     car = Car(self, np.array([c, px]), np.array([0.0, -1.0]))
+        #     self.space.place_agent(car, car.pos)
+        #     self.schedule.add(car)
+
+        d = 10
+        for px in np.random.choice(1, ncarros, replace=True):
+            car = Car(self, np.array([px, d]), np.array([-1.0, 0.0]))
+            self.space.place_agent(car, car.pos)
+            self.schedule.add(car)
 
     def step(self):
         self.schedule.step()
 
 def car_draw(agent):
     color = "Blue" if agent.unique_id == 1 else "Brown"
-    if type(agent) == Car:
-        return {"Shape": "rect", "w": 0.034, "h": 0.02, "Filled": "true", "Color": color}
+    return {"Shape": "rect", "w": 0.034, "h": 0.02, "Filled": "true", "Color": color}
 
 canvas = SimpleCanvas(car_draw, 500, 500)
 
@@ -106,4 +89,3 @@ model_params = {}
 # server = ModularServer(Street, [canvas], "Traffic2", model_params)
 # server.port = 8522
 # server.launch()
-
